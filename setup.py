7281c7935537a5980be0bf092f45c5290ad0a532
DainiusSaltenis <saltenisdainius97@gmail.com>
2024-09-30 19:06:02 +0200

    add missing package

--- setup.py
+++ setup.py
@@ -4,18 +4,19 @@ setup(
     name="florence2-client",
     version="0.1.0",
     description="Florence-2 Client and Server Package",
-    packages=find_packages(include=["florence2_client", "florence2_server", "florence2_client.*", "florence2_server.*"]),
+    packages=find_packages(include=["florence2_client", "florence2_client.*"]),
     install_requires=[
         "fastapi",
         "uvicorn",
         "requests",
+        "resources"
         "pillow",
         "transformers",
         "torch",
         "supervision"
     ],
     extras_require={
-        "client": ["requests", "supervision", "pillow"],
+        "client": ["requests", "supervision", "pillow", "resources"],
         "server": ["fastapi", "uvicorn", "pillow", "transformers", "torch"],
     },
     python_requires=">=3.10"
